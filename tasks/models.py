28642b67fd74922286ca688e874df462f94facd7
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-08-02 00:44:08 +0200

    created task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        (1, 'Urgent'),
+        (2, 'Medium'),
+        (3, 'Low'),
+    ]
+    
+    CATEGORY_CHOICES = [
+        ('Technical Task', 'Technical Task'),
+        ('User Story', 'User Story'),
+    ]
+    
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
+    due_date = models.DateField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=20)
+    assigned_to = models.JSONField(default=list)
+    subtasks = models.JSONField(default=list)
+    
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+class TaskModelTest(TestCase):
+    
+    def test_task_model_exists(self):
+        tasks_count = Task.objects.count()
+        self.assertEqual(tasks_count, 0)
