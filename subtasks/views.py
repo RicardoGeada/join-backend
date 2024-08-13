03f8c11d072d79600258042ee224811c7f49c714
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-08-13 23:53:41 +0200

    serializer and view

--- subtasks/views.py
+++ subtasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Subtask
+from .serializers import SubtaskSerializer
 
 # Create your views here.
+class SubtaskViewSet(viewsets.ModelViewSet):
+    queryset = Subtask.object.all()
+    serializer_class = SubtaskSerializer
+    permission_classes = [IsAuthenticated]
+    
