4ca4bfffd90a8b44233ab44c2737dc0761c9f5c1
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-08-18 10:39:41 +0200

    subtasks tests

--- subtasks/serializers.py
+++ subtasks/serializers.py
@@ -6,3 +6,9 @@ class SubtaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subtask
         fields = '__all__'
+            
+            
+    def update(self, instance, validated_data):
+        validated_data.pop('task', None)
+        return super().update(instance, validated_data)
+        
