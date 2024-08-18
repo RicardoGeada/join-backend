71c69f5539216f8e96846327fac9eb6e1875c197
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-08-19 01:21:59 +0200

    test subtasks

--- subtasks/views.py
+++ subtasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from .models import Subtask
 from .serializers import SubtaskSerializer
 
@@ -9,4 +10,5 @@ class SubtaskViewSet(viewsets.ModelViewSet):
     queryset = Subtask.objects.all()
     serializer_class = SubtaskSerializer
     permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
     
